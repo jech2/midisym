de54294206c64bb278465a108c3d1407e77fb288
Eunjin Choi <jech@kaist.ac.kr>
2025-10-31 13:08:18 +0900

    update crop midi

--- midisym/parser/utils.py
+++ midisym/parser/utils.py
@@ -1,6 +1,7 @@
 from .container import SymMusicContainer
 from typing import Dict
 import numpy as np
+import copy
 
 def seconds_to_ticks(seconds, ticks_per_beat, tempo, tempo_ref='microseconds'):
     """Convert seconds to MIDI ticks."""
@@ -102,3 +103,50 @@ def resample_ticks_per_beat(sym_obj: SymMusicContainer, ticks_per_beat: int):
     sym_obj.max_tick = sym_obj.max_tick * ticks_per_beat // ori_ticks_per_beat
     
     return sym_obj 
+
+def crop_midi_obj(midi_parser, start, end, select_inst=None, unit='tick'):
+    if unit == 'tick':
+        start_tick = start
+        end_tick = end
+    elif unit == 'bar':
+        tpb = midi_parser.sym_music_container.ticks_per_beat
+        tick_per_bar = tpb * 4
+        start_tick = start * tick_per_bar
+        end_tick = end * tick_per_bar
+    else:
+        raise ValueError(f"unit must be 'tick' or 'bar'. Got {unit}")
+
+    new_parser = copy.deepcopy(midi_parser)
+    if select_inst is not None:
+        new_parser.sym_music_container.instruments = [new_parser.sym_music_container.instruments[i] for i in select_inst]
+
+    new_midi_obj = new_parser.sym_music_container
+    
+    # note related events only
+    for i, instrument in enumerate(new_midi_obj.instruments):
+        new_midi_obj.instruments[i].notes = [note for note in instrument.notes if start_tick <= note.start < end_tick and start_tick < note.end <= end_tick]
+        
+    # shift the note from the start_tick
+    for i, instrument in enumerate(new_midi_obj.instruments):
+        for note in instrument.notes:
+            note.start -= start_tick
+            note.end -= start_tick
+            
+    # marker related events only
+    selected_markers = [marker for marker in new_midi_obj.markers if start_tick <= marker.time < end_tick]
+    for marker in selected_markers:
+        marker.time -= start_tick
+
+    # 맨 처음 마커가 없으면 추가
+    if len(selected_markers) > 0 and selected_markers[0].time > new_midi_obj.ticks_per_beat / 2:
+        for marker in new_midi_obj.markers:
+            if marker.time < selected_markers[0].time:
+                first_marker = marker
+                first_marker.time = 0
+                break
+
+        selected_markers.insert(0, first_marker)
+    new_midi_obj.markers = selected_markers
+    new_midi_obj.max_tick = (end_tick - start_tick)
+        
+    return new_parser
